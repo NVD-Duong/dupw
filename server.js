5a507eddb80fccff664f70359a6b97632ac89d1b
duongnot <nongvanduongdv@gmail.com>
2024-06-08 22:05:37 +0700

    commit 3

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const { createServer } = require("node:http");
+const express = require("express");
+const app = express();
+const port = 8080;
 
-const hostname = "127.0.0.1";
-const port = 3000;
-
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World nong van duong");
+app.get("/", (req, res) => {
+  res.send("Hello World! NÔNG van dương ");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
